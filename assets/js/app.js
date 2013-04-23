c2a1a24eeef2d8eeb488f0c40ee4fadf5ff0679d
Artur Pokusin <artur@pokusin.com>
2013-04-22 22:27:18 -0700

    Added whole bunch of styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,42 @@
 $(document).ready(function(){
-  
-  // Write your Javascript!
+
+ // Loading the latest profile JSON from Preserve.
+  $.getJSON('http://preserve.io/u/artur.json', function(data){
+    var items = [];
+    // How many bookmarks are we going to display?
+    var limit = 5;
+    var bookmarks = 0;
+    $.each(data.user.bookmarks, function(key, bookmark){
+      if(bookmarks > limit) {
+        return false;
+      }
+      else {
+        bookmarks = bookmarks + 1;
+        items.push('<a target="_blank" href="'+ bookmark.url +'">'+ bookmark.title +'</a>');
+      }
+    });
+    $('#bookmarks').html(items.join(''));
+  });
+
+  // API Ref: http://api.dribbble/players/:id/shots
+  $.jribbble.getShotsByPlayerId('apokusin', function (playerShots) {
+    var html = [];
+    $.each(playerShots.shots, function (i, shot) {
+      html.push('<a target="_blank" href="' + shot.url + '">');
+      html.push('<img src="' + shot.image_teaser_url + '" ');
+      html.push('alt="' + shot.title + '"></a></li>');
+    });
+
+    $('#dribbble').html(html.join(''));
+  }, {page: 1, per_page: 6});
+
+  // Flickr Photos
+  $('#flickr').jflickrfeed({
+      limit: 6,
+      qstrings: {
+          id: '95116542@N02'
+      },
+      itemTemplate: '<a target="_blank" href="{{image_b}}"><img alt="{{title}}" src="{{image_m}}" /></a>'
+  });
 
 });
